b4dbf8364533a919162d7c094aeddc87f187f2f9
Ryan Brunner <ryan@influitive.com>
2014-03-19 07:46:17 -0400

    Initial version - controllers and new route

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hacklaunch::Application.config.secret_key_base = '04c694853ac3c4c729afa212c85fdefe1a7c6ffc00ffd377b24fb41aeace2eb3d60a7af74b928cdcc18ee1e48516412ecbea2f5018081ba39d610d58cfc68a6d'
