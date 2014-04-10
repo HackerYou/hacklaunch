fd00743b2fffcb78085387e2e7f4610aa57ff521
Ryan Brunner <ryan@influitive.com>
2014-04-10 19:29:40 -0400

    REST stuff

--- app/controllers/sign_ups_controller.rb
+++ app/controllers/sign_ups_controller.rb
@@ -2,16 +2,34 @@ class SignUpsController < ApplicationController
 
   def new
     @sign_up_count = SignUp.count
+    @sign_up = SignUp.new
   end
 
   def create
-    @sign_up = SignUp.new
-    @sign_up.name = params[:name]
-    @sign_up.email = params[:email]
-    @sign_up.save
+    @sign_up = SignUp.new(sign_up_params)
+    if @sign_up.save
+      redirect_to sign_ups_path
+    else
+      flash[:error] = @sign_up.errors.full_messages.join(", ")
+      render :new
+    end
+  end
+
+  def edit
+    @sign_up = SignUp.find(params[:id])
+  end
+
+  def update
+    @sign_up = SignUp.find(params[:id])
+    @sign_up.update(sign_up_params)
+    redirect_to sign_ups_path
   end
 
   def index
     @sign_ups = SignUp.all
+  end
+
+  def sign_up_params
+    params.require(:sign_up).permit(:name, :email)
   end
 end
--- app/models/sign_up.rb
+++ app/models/sign_up.rb
@@ -1,2 +1,6 @@
 class SignUp < ActiveRecord::Base
+
+  validates :email, presence: {message: "is blank"}, uniqueness: true
+  validates :name, presence: true
+
 end
